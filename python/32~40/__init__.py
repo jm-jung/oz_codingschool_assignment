5ee90ef774ad8a6031ea72341523cc00cacf6fbe
jm-jung <jmsk7554@gmail.com>
2024-07-06 01:08:57 +0900

    Add files via upload

---
+++ python/32~40/__init__.py
@@ -0,0 +1,4 @@
+import animals.birds as 새
+import animals.mammals as 포유류
+print(새.birds)
+print(포유류.mammals)
---
+++ python/32~40/mymath.py
@@ -0,0 +1,9 @@
+pi=3.14
+def triangle(x,y):
+    return x*y/2
+
+def square(x,y):
+    return x*y
+
+def circle(radius):
+    return pi*radius**2
